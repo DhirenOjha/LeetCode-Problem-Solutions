6e1f07fd17a6c9e4e04678db38250a44e61ed7cb
Dhiren Ojha <81586420+DhirenOjha@users.noreply.github.com>
2023-01-04 18:00:40 +0530

    Time: 44 ms (69.39%), Space: 94.4 MB (57.22%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -1,54 +1,12 @@
 class Solution {
     public int minimumRounds(int[] tasks) {
-        Arrays.sort(tasks);
-//         System.out.println(Arrays.toString(tasks));
-//         int rounds=0;
-//         int arr[]=new int[tasks.length];
-//         System.out.println(arr.length);
-//         for(int i=0;i<arr.length-1;i++)
-//         {
-//             arr[tasks[i]]++;
-//         }
-//         System.out.println(Arrays.toString(arr));
-        
-//         for(int i=0;i<arr.length-1;i++)
-//         {
-//             if(arr[i]==2||arr[i]==3)
-//             {
-//                 System.out.println("T");
-//                 rounds++;
-//             }
-//                 else if(arr[i]==1)
-//             {
-//                 System.out.println("TT");
-//                 return -1;
-//             }
-//             else if(arr[i]==0)
-//                 continue;
-//             else
-//             {
-//                 int mul=arr[i]/3;
-//                 rounds+=mul;
-//                 arr[i]-=3*mul;
-//                 System.out.println(arr[i]);
-//                 System.out.println(mul);
-//                 if(arr[i]==1)
-//                     rounds++;
-//                 else if(arr[i]==2)
-//                     rounds++;
-//             }
-                
-//         }
-//         return rounds;
-        
-        System.out.println(Arrays.toString(tasks));
+        Arrays.sort(tasks);    
         int round=0;
         int count=0;
         for(int i=0;i<tasks.length-1;i++)
         {
             if(tasks[i]==tasks[i+1])
             {
-                System.out.println("Yes2");
                 count++;
             }
             else
@@ -56,12 +14,10 @@ class Solution {
                 count++;
                 if(count==2||count==3)
                 {
-                    System.out.println("Yes1");
                     round++;
                 }
                 else if(count>3)
                 {
-                    System.out.println("Yes");
                     int dif=count/3;
                     round=round+dif;
                     if(count%3==1||count%3==2)
@@ -69,7 +25,6 @@ class Solution {
                 }
                 else //count==1;
                     return -1;
-                System.out.println(count);
                 count=0;
             }
         }
@@ -78,12 +33,10 @@ class Solution {
                 count++;
                 if(count==2||count==3)
                 {
-                    System.out.println("Yes1");
                     round++;
                 }
                 else if(count>3)
                 {
-                    System.out.println("Yes");
                     int dif=count/3;
                     round=round+dif;
                     if(count%3==1||count%3==2)
@@ -97,7 +50,6 @@ class Solution {
         if(tasks[tasks.length-1]!=tasks[tasks.length-2])
             return -1;
         else
-            
-        return round;
+            return round;
     }
 }
