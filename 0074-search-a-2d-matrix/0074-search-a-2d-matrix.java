8bb79cabe1aa4e2c3868ac6a3f6a917d2a33865e
Dhiren Ojha <81586420+DhirenOjha@users.noreply.github.com>
2023-01-25 14:25:30 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (58.72%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean searchMatrix(int[][] mat, int target) {
+        // ArrayList<Integer> arr=new ArrayList<>();
+        // for(int i=0;i<mat.length;i++)
+        // {
+        //     for(int j=0;j<mat[i].length;j++)
+        //     {
+        //         arr.add(mat[i][j]);
+        //     }
+        // }
+        // System.out.println(arr);
+        // int s=0,e=arr.size();
+        // while(s<=e)
+        // {
+        //     int mid=s+(e-s)/2;
+        //     if(arr.get(mid)>target)
+        //         e=mid-1;
+        //     else if(arr.get(mid)<target)
+        //         s=mid+1;
+        //     else
+        //         return true;
+        // }
+        // return false;
+        
+        int i=0,j=mat[0].length-1;
+        while(i<mat.length&&j>=0)
+        {
+            if(mat[i][j]>target)
+                j--;
+            else if(mat[i][j]<target)
+                i++;
+            else
+                return true;
+        }
+        return false;
+    }
+}
