112a0db04ed02b653b68fa8e34b4b751ca6c3e8d
Dhiren Ojha <81586420+DhirenOjha@users.noreply.github.com>
2023-01-30 23:55:06 +0530

    Time: 8 ms (86.26%), Space: 43.2 MB (74.00%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -2,15 +2,9 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        System.out.println(Arrays.toString(g));
-        System.out.println(Arrays.toString(s));
         int count=0,num;
         if(s.length==0||g.length==0)
             return count;
-        if(s.length<g.length)
-            num=g.length;
-        else
-            num=s.length;
         int g1=0;
         for(int i=0;i<s.length;i++)
         {
